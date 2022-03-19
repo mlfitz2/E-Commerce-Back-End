db9ff1fb3134d9ad08f191199c8a5b9beb10cb32
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-03-19 15:55:31 -0700

    database models defined

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    tag: {
+      id: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },
+    
   },
   {
     sequelize,
